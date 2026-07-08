40b7858188c89f90749c28819119239927d27d7c
yash-probus <guptayash2804@gmail.com>
2026-07-08 11:54:20 +0530

    fix: increase express body size limit to 50mb

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -35,8 +35,8 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Body parser, reading data from body into req.body
-app.use(express.json({ limit: '10kb' }));
-app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 import authRoutes from './modules/auth/auth.routes';
 import uploadRoutes from './modules/upload/upload.routes';
