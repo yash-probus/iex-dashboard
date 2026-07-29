640c1fb0ca6b2bbcef1fbb66f80b2b7203f7ae46
yash-probus <guptayash2804@gmail.com>
2026-07-29 16:20:34 +0530

    Change frontend container port to 8085 and update Nginx script

--- backend/nginx_update.sh
+++ backend/nginx_update.sh
@@ -7,7 +7,7 @@ server {
 
     # Frontend routing
     location / {
-        proxy_pass http://127.0.0.1:8081;
+        proxy_pass http://127.0.0.1:8085;
         proxy_http_version 1.1;
         proxy_set_header Upgrade $http_upgrade;
         proxy_set_header Connection 'upgrade';
