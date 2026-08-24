b8c8999cbbfcd4e47d844f54c49787e783a07c90
yash-probus <guptayash2804@gmail.com>
2026-08-25 00:22:15 +0530

    Fix file download name bug by appending link to document body

--- frontend/src/utils/downloadBlob.ts
+++ frontend/src/utils/downloadBlob.ts
@@ -4,6 +4,9 @@ export const downloadBlob = (blob: Blob, filename: string) => {
   link.rel = 'noopener';
   link.href = window.URL.createObjectURL(blob);
   
+  // Must append to body for the download attribute to be respected by modern browsers
+  document.body.appendChild(link);
+  
   setTimeout(() => window.URL.revokeObjectURL(link.href), 40000); // 40s timeout just like file-saver
   
   setTimeout(() => {
@@ -13,6 +16,10 @@ export const downloadBlob = (blob: Blob, filename: string) => {
       const evt = document.createEvent('MouseEvents');
       evt.initMouseEvent('click', true, true, window, 0, 0, 0, 80, 20, false, false, false, false, 0, null);
       link.dispatchEvent(evt);
+    }
+    // Clean up
+    if (document.body.contains(link)) {
+      document.body.removeChild(link);
     }
   }, 0);
 };
