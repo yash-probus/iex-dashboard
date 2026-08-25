4c4f529486cfdb73df3c89a1bbf598e123accc04
yash-probus <guptayash2804@gmail.com>
2026-08-25 16:31:46 +0530

    chore: remove test scripts, temporary data, and generated artifacts

--- parse_pdf.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const pdf = require('pdf-parse');
-let dataBuffer = fs.readFileSync('Rajeev_Jaiswal_Energy_Savings_Report_Redesigned.pdf');
-pdf(dataBuffer).then(function(data) {
-    console.log(data.text.substring(0, 1500));
-});
