54c3f5fc32abe4eb1284c7fce872256448b9084b
yash-probus <guptayash2804@gmail.com>
2026-09-16 19:24:47 +0530

    y

--- backend/src/modules/trader-performance-actual/trader-performance-actual.service.ts
+++ backend/src/modules/trader-performance-actual/trader-performance-actual.service.ts
@@ -637,7 +637,7 @@ export class TraderPerformanceActualService {
     let totalMarketEnergyKwh = 0;
     let totalConsumerBusEnergyKwh = 0;
 
-    let monthsToProcess = Object.entries(todConsumptions).filter(([ym]) => ym.match(/^\d{4}-\d{2}$/));
+    let monthsToProcess = Object.entries(todConsumptions).filter(([ym]) => /^\d{4}-\d{2}$/.test(ym));
     if (targetMonth) {
       monthsToProcess = monthsToProcess.filter(([ym]) => ym === targetMonth);
       if (monthsToProcess.length === 0) {
