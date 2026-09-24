0aeb59947d862f0cb36694da5a893e043c751cf0
yash-probus <guptayash2804@gmail.com>
2026-09-24 11:12:04 +0530

    chore: remove dead code and scratch files

--- backend/test_jsdoc.ts
+++
@@ -1,7 +0,0 @@
-import swaggerJsdoc from 'swagger-jsdoc';
-const options = {
-  definition: { openapi: '3.0.0', info: { title: 'Test', version: '1.0.0' } },
-  apis: ['./src/modules/up-market/*.ts']
-};
-const spec = swaggerJsdoc(options);
-console.log(JSON.stringify(spec, null, 2));
